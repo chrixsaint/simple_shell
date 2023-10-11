272863d6048796f9fbd4589a7c548f5097253a2c
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 14:31:20 +0100

    Checks if a character is alphabetic (a-z or A-Z).

--- checkAlphabetic.c
+++ checkAlphabetic.c
@@ -1,16 +1,23 @@
 #include "shell.h"
 
 /**
- *checkAlphabetic - checks for alphabetic character
- *@c: The character to input
- *Return: 1 if c is alphabetic, 0 otherwise
- *_atoic.c
+ * checkAlphabetic - Checks if a character is alphabetic (a-z or A-Z).
+ * @c: The character to be checked.
+ *
+ * This function checks if the character 'c' is an alphabetic
+ * character, meaning it falls within the range 'a' to 'z' or 'A' to 'Z'.
+ *
+ * Return: 1 if 'c' is an alphabetic character, 0 otherwise.
  */
 
 int checkAlphabetic(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
