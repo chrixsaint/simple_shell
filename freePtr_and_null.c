ac88e82bc40679c732e96df09e485575318e0ab8
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 17:16:55 +0100

    Frees a pointer and sets it to NULL.

--- freePtr_and_null.c
+++ freePtr_and_null.c
@@ -1,12 +1,16 @@
 #include "shell.h"
 
 /**
- * freePtr_and_null - frees a pointer and NULLs the address
- * @ptr: address of the pointer to free
+ * freePtr_and_null - Frees a pointer and sets it to NULL.
+ * @ptr: The address of the pointer to be freed and set to NULL.
  *
- * Return: 1 if freed, otherwise 0.
- * memory.c
+ * This function frees the memory pointed to by the given pointer
+ * and then sets the pointer itself to NULL to
+ * prevent further access to the freed memory.
+ *
+ * Return: 1 if the pointer was successfully freed, otherwise 0.
  */
+
 int freePtr_and_null(void **ptr)
 {
 	if (ptr && *ptr)
