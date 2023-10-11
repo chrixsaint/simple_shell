a5ad927f4bc270364866d1b1b105d5c0fe58271b
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 17:27:16 +0100

    Returns a string array copy

--- get_environ.c
+++ get_environ.c
@@ -1,15 +1,19 @@
 #include "shell.h"
 
 /**
- * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- * getenv.c
+ * get_environ - Returns a string array copy
+ * of the environment variables.
+ * @info: The parameter struct containing potential arguments.
+ *
+ * This function retrieves the environment variables stored in
+ * the parameter struct and returns a copy of them as a string array.
+ *
+ * Return: A string array containing environment variable strings.
  */
+
 char **get_environ(info_t *info)
 {
-	if (!info->environ || info->env_changed)
+	if (info->environ == NULL || info->env_changed)
 	{
 		info->environ = list_to_strings(info->env);
 		info->env_changed = 0;
