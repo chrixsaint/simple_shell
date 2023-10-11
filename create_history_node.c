2bf2a30b3fbe0db9248cb19630513b1c353323fe
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 15:55:33 +0100

    Adds a new entry to a history linked list.

--- create_history_node.c
+++ create_history_node.c
@@ -1,24 +1,32 @@
 #include "shell.h"
 
-
 /**
- * create_history_node - adds entry to a history linked list
+ * create_history_node - Adds a new entry to a history linked list.
  * @info: Structure containing potential arguments. Used to maintain
- * @buf: buffer
- * @linecount: the history linecount, histcount
+ * @buf: The input buffer to be added to the history.
+ * @linecount: The history line count, histcount.
  *
- * Return: Always 0
- * history.c
+ * This function adds a new entry with the provided input buffer
+ * `buf` and history line count `linecount`
+ * to the history linked list stored in the `info` structure.
+ *
+ * Return: Always 0.
  */
+
 int create_history_node(info_t *info, char *buf, int linecount)
 {
-	list_t *node = NULL;
+	list_t *nnodde;
 
+	nnodde = NULL;
 	if (info->history)
-		node = info->history;
-	add_node_to_tail(&node, buf, linecount);
+	{
+		nnodde = info->history;
+	}
+	add_node_to_tail(&nnodde, buf, linecount);
 
-	if (!info->history)
-		info->history = node;
+	if (info->history == NULL)
+	{
+		info->history = nnodde;
+	}
 	return (0);
 }
