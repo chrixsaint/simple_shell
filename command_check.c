8e81e96123264af5ec20e0014115490bbf038398
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 14:40:45 +0100

    Determines if a file is an executable command.

--- command_check.c
+++ command_check.c
@@ -1,24 +1,29 @@
 #include "shell.h"
 
+
 /**
- * command_check - determines if a file is an executable command
- * @info: the info struct
- * @path: path to the file
+ * command_check - Determines if a file is an executable command.
+ * @info: Structure containing potential arguments.
+ * Not used in this function.
+ * @path: Path to the file to check.
  *
- * Return: 1 if true, 0 otherwise
- * parser.c
+ * This function checks if the file specified by
+ * the given path is a regular file and returns 1
+ * if it is executable; otherwise, it returns 0.
+ *
+ * Return: 1 if the file is executable, 0 otherwise.
  */
 int command_check(info_t *info, char *path)
 {
-	struct stat st;
+	struct stat strin;
 
 	(void)info;
-	if (!path || stat(path, &st))
-		return (0);
-
-	if (st.st_mode & S_IFREG)
+	if (!path || stat(path, &strin))
 	{
-		return (1);
+		return (0);
 	}
+
+	if (strin.st_mode & S_IFREG)
+		return (1);
 	return (0);
 }
