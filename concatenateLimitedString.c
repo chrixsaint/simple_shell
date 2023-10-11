e585cefa563d6351b259431dc11e067085b9f889
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 15:18:47 +0100

    Concatenates two strings with a length limit.

--- concatenateLimitedString.c
+++ concatenateLimitedString.c
@@ -2,28 +2,37 @@
 
 
 /**
- **concatenateLimitedString - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
+ * concatenateLimitedString - Concatenates two strings with a length limit.
+ * @dest: The destination string where the result will be stored.
+ * @src: The source string to be concatenated.
+ * @n: The maximum number of characters from src to concatenate.
+ *
+ * This function appends up to `n` characters from the source
+ * string `src` to the end of the destination string `dest`.
+ * It ensures the resulting string remains null-terminated and
+ * does not exceed the allocated buffer size.
+ *
+ * Return: A pointer to the destination string `dest`.
  */
+
 char *concatenateLimitedString(char *dest, char *src, int n)
 {
-	int i, j;
-	char *s = dest;
+	char *s;
+	int aii, jai;
 
-	i = 0;
-	j = 0;
-	while (dest[i] != '\0')
-		i++;
-	while (src[j] != '\0' && j < n)
+	s = dest;
+	jai = 0;
+	for (aii = 0; dest[aii] != '\0'; aii++)
+		;
+	while (src[jai] != '\0' && jai < n)
 	{
-		dest[i] = src[j];
-		i++;
-		j++;
+		dest[aii] = src[jai];
+		aii++;
+		jai++;
 	}
-	if (j < n)
-		dest[i] = '\0';
+	if (jai < n)
+	{
+		dest[aii] = '\0';
+	}
 	return (s);
 }
