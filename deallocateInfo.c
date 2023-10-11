a33b3ce4a1b881158618ef06e7af7b146671ec28
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 16:08:41 +0100

    Frees the fields of the info_t struct as specified.

--- deallocateInfo.c
+++ deallocateInfo.c
@@ -2,11 +2,17 @@
 
 
 /**
- * deallocateInfo - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
+ * deallocateInfo - Frees the fields of the info_t struct as specified.
+ * @info: Pointer to the info_t struct.
+ * @all: True if freeing all fields, otherwise, only specific fields are freed.
+ *
+ * This function deallocates memory for specific fields
+ * within the info_t struct.
+ *
+ * Return: void.
  * getinfo.c
  */
+
 void deallocateInfo(info_t *info, int all)
 {
 	free_string_array(info->argv);
@@ -14,11 +20,13 @@ void deallocateInfo(info_t *info, int all)
 	info->path = NULL;
 	if (all)
 	{
-		if (!info->cmd_buf)
+		if (info->cmd_buf == NULL)
+		{
 			free(info->arg);
-		if (info->env)
+		}
+		if (info->env != NULL)
 			deallocate_list(&(info->env));
-		if (info->history)
+		if (info->history != NULL)
 			deallocate_list(&(info->history));
 		if (info->alias)
 			deallocate_list(&(info->alias));
@@ -26,7 +34,9 @@ void deallocateInfo(info_t *info, int all)
 			info->environ = NULL;
 		freePtr_and_null((void **)info->cmd_buf);
 		if (info->readfd > 2)
+		{
 			close(info->readfd);
+		}
 		write_char(BUF_FLUSH);
 	}
 }
