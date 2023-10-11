dcb2e48b40e9c1d88a1f80739d66616b92dd0fae
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 12:50:57 +0100

    Adds a node to the start of the linked list.

--- add_node_to_head.c
+++ add_node_to_head.c
@@ -1,35 +1,41 @@
 #include "shell.h"
 
 /**
- * add_node_to_head - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * add_node_to_head - Adds a node to the start of the linked list.
+ * @head: Address of a pointer to the head node.
+ * @str: The string to be stored in the new node.
+ * @num: Node index used by history (if applicable).
  *
- * Return: size of list
- * lists.c
+ * This function creates a new node and inserts it at the start of the list.
+ *
+ * Return: A pointer to the newly created node, or NULL on failure.
  */
+
 list_t *add_node_to_head(list_t **head, const char *str, int num)
 {
-	list_t *new_head;
+	list_t *new_node;
 
-	if (!head)
+	if (head == NULL)
+	{
 		return (NULL);
-	new_head = malloc(sizeof(list_t));
-	if (!new_head)
+	}
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
 		return (NULL);
-	assign_memory((void *)new_head, 0, sizeof(list_t));
-	new_head->num = num;
+	}
+	assign_memory((void *)new_node, 0, sizeof(list_t));
+	new_node->num = num;
 	if (str)
 	{
-		new_head->str = duplicate_strng(str);
-		if (!new_head->str)
+		new_node->str = duplicate_strng(str);
+		if (new_node->str == NULL)
 		{
-			free(new_head);
+			free(new_node);
 			return (NULL);
 		}
 	}
-	new_head->next = *head;
-	*head = new_head;
-	return (new_head);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
