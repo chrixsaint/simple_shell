571b7313572ec3dab9272cc6dcce688414fe5e8d
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 16:34:05 +0100

    Checks if a character is a delimiter by

--- delimiterCheck.c
+++ delimiterCheck.c
@@ -2,16 +2,24 @@
 
 
 /**
- * delimiterCheck - checks if character is a delimeter
- * @c: the char to check
- * @delim: the delimeter string
- * Return: 1 if true, 0 if false
+ * delimiterCheck - Checks if a character is a delimiter by
+ * comparing it to a given delimiter string.
+ * @c: The character to check.
+ * @delim: The delimiter string containing delimiting characters.
+ *
+ * This function checks if the character `c` is
+ * present in the delimiter string `delim`.
+ *
+ * Return: 1 if `c` is a delimiter, 0 if not.
  * _atoic.c
  */
+
 int delimiterCheck(char c, char *delim)
 {
 	while (*delim)
 		if (*delim++ == c)
+		{
 			return (1);
+		}
 	return (0);
 }
