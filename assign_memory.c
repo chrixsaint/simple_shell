c1d9ffc42519c79653e5c23cb5b5d23c7e91b191
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 13:40:25 +0100

    Fills a memory area with a constant byte.

--- assign_memory.c
+++ assign_memory.c
@@ -3,18 +3,26 @@
 #include "shell.h"
 
 /**
- **assign_memory - fills memory with a constant byte
- *@s: the pointer to the memory area
- *@b: the byte to fill *s with
- *@n: the amount of bytes to be filled
- *Return: (s) a pointer to the memory area s
- *realloc.c
+ * assign_memory - Fills a memory area with a constant byte.
+ * @s: Pointer to the memory area to be filled.
+ * @b: The byte to fill the memory area with.
+ * @n: The number of bytes to be filled.
+ *
+ * This function is used to fill a memory area
+ * pointed to by 's' with 'n' bytes of the constant byte 'b'.
+ *
+ * Return: A pointer to the memory area 's' after filling.
  */
+
 char *assign_memory(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	unsigned int q;
 
-	for (i = 0; i < n; i++)
-		s[i] = b;
+	q = 0;
+	while (q < n)
+	{
+		s[q] = b;
+		q++;
+	}
 	return (s);
 }
