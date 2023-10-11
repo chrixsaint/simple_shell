a2e9d278383294b6a6aa906084a81a22200e577e
christian ochenehi peter <christianochenehipeter@gmail.com>
2023-10-11 13:31:32 +0100

    Adds a node to the end of a linked list.

--- aliasHandler.c
+++ aliasHandler.c
@@ -1,35 +1,48 @@
 #include "shell.h"
 
+
 /**
- * aliasHandler - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
- * builtin1.c
+ * aliasHandler - Handles alias commands to mimic
+ * the behavior of the alias builtin (man alias).
+ * @info: Structure containing potential arguments.
+ * Used to maintain a consistent function prototype.
+ *
+ * This function handles alias commands, allowing users to
+ * view, set, or modify aliases.
+ * If called with no arguments, it displays all existing aliases.
+ * If called with arguments in the form of 'alias name=value',
+ * it creates or modifies an alias.
+ * If called with an alias name, it displays the alias value.
+ *
+ * Return: Always 0
  */
 int aliasHandler(info_t *info)
 {
-	int i = 0;
+	int q = 0;
 	char *p = NULL;
-	list_t *node = NULL;
+	list_t *n = NULL;
 
+	q = 1;
 	if (info->argc == 1)
 	{
-		node = info->alias;
-		while (node)
+		n = info->alias;
+		while (n)
 		{
-			printAliasString(node);
-			node = node->next;
+			printAliasString(n);
+			n = n->next;
 		}
 		return (0);
 	}
-	for (i = 1; info->argv[i]; i++)
+	while (info->argv[q])
 	{
-		p = searchCharInStr(info->argv[i], '=');
+		p = searchCharInStr(info->argv[q], '=');
 		if (p)
-			initializeAlias(info, info->argv[i]);
+		{
+			initializeAlias(info, info->argv[q]);
+		}
 		else
-			printAliasString(node_starts_with(info->alias, info->argv[i], '='));
+			printAliasString(node_starts_with(info->alias, info->argv[q], '='));
+			q++;
 	}
 
 	return (0);
